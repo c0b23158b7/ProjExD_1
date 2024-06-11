99313544f822b08331b69e758091cf670f67978c
yuki <c0b23158b7@edu.teu.ac.jp>
2024-06-11 16:53:10 +0900

    こうかとん右移動、キー操作

--- pygame_image.py
+++ pygame_image.py
@@ -10,21 +10,33 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
 
+    kk_rct = kk_img.get_rect() #こうかとんRectの抽出
+    kk_rct.center = 300, 200    
     tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        kk_rct = kk_img.get_rect()
-        kk_rct.center = 300, 200
-        screen.blit(kk_img,[300, 200])
-
+        
+        x = tmr%3200
+        
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img, [-x+4800, 0])
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
