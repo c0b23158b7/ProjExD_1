847ca10c7f9bfe8e0249cfec043f5f8f25c66748
yuki <c0b23158b7@edu.teu.ac.jp>
2024-06-11 18:26:08 +0900

    pygame_image.pyからflying_kokaton.pyに再変更

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -26,19 +26,26 @@ def main():
         x = tmr%3200
         
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img, [-x+4800, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
         
         key_lst = pg.key.get_pressed()
+        
+        Px = 0
+        Py = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            Py -= 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            Py += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, +1)
+            Px -= 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
+            Px += 2
+        
+        Px += -1
+        
+        kk_rct.move_ip(Px, Py)
         
         screen.blit(kk_img, kk_rct)
         pg.display.update()
