2de0e181bb3878a2755e88796ebe8d41ce57f409
yuki <c0b23158b7@edu.teu.ac.jp>
2024-06-11 16:10:46 +0900

    こうかと追加

---
+++ pygame_image.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        kk_rct = kk_img.get_rect()
+        kk_rct.center = 300, 200
+        screen.blit(kk_img,[300, 200])
+
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
