13f1883d457c565d868db604cd8da91dcb369f3b
yuki <c0b23158b7@edu.teu.ac.jp>
2024-06-11 17:57:23 +0900

    背景画像ループ、左に流れる、→で右に倍速移動、move_ipを１回のみ使用に変更

---
+++ pygame_kokaton.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+
+    kk_rct = kk_img.get_rect() #こうかとんRectの抽出
+    kk_rct.center = 300, 200    
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        
+        x = tmr%3200
+        
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+        
+        key_lst = pg.key.get_pressed()
+        
+        Px = 0
+        Py = 0
+        if key_lst[pg.K_UP]:
+            Py -= 1
+        if key_lst[pg.K_DOWN]:
+            Py += 1
+        if key_lst[pg.K_LEFT]:
+            Px -= 1
+        if key_lst[pg.K_RIGHT]:
+            Px += 2
+        kk_rct.move_ip(Px, Py)
+        
+        
+        kk_rct.move_ip(-1, 0)
+        
+        screen.blit(kk_img, kk_rct)
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
